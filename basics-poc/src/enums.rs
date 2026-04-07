5acde7a9150862015936f6037a96eb04af15667b
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-07 09:01:44 -0300

    feat: update enums

--- basics-poc/src/enums.rs
+++ basics-poc/src/enums.rs
@@ -38,6 +38,12 @@ pub fn enums() {
         Student => println!("Students are acquiring knowledge!"),
         Teacher => println!("Teachers are spreading knowledge!"),
     }
+
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as u32);
+    println!("violets are #{:06x}", Color::Blue as u32);
 }
 
 enum WebEvent {
@@ -87,4 +93,16 @@ enum Stage {
 enum Role {
     Student,
     Teacher,
+}
+
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
 }
