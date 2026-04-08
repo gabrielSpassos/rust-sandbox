8a67b20dbed669654146055c925478cbbcdd1a64
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-08 08:26:47 -0300

    feat: add variables poc

--- basics-poc/src/variables.rs
+++ basics-poc/src/variables.rs
@@ -11,4 +11,52 @@ pub fn variables() {
     println!("Meet the unit value: {:?}", unit);
 
     let _unused_variable = 3u32;
+
+    mutable_variables();
+    scope_variables();
+    shadowing_variables();
+}
+
+fn mutable_variables() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+}
+
+fn scope_variables() {
+    let long_lived_binding = 1;
+
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+        println!("inner long: {}", long_lived_binding);
+    }
+
+    // println!("outer short: {}", short_lived_binding);
+
+    println!("outer long: {}", long_lived_binding);
+}
+
+fn shadowing_variables() {
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
 }
